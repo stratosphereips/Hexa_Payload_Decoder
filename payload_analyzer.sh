9f2e7b993a37ad54297dc1d52c3cb716c687b3ce
Maria Rigaki <mrigaki@gmail.com>
2022-02-06 11:57:10 +0100

    Added libre translator support to replace Google

--- payload_analyzer.sh
+++ payload_analyzer.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#This script goal is to decode any hex data and translate it to english.
-#To do so it uses javascript CyberChef script and Googletrans python library.
-#Javascript: It's necessary to have node and npm installed and  CyberChef Node-Api installed found at: https://github.com/gchq/CyberChef/wiki/Node-API
-#Python3: It's also necessary to have Googletrans python3 library installed found at: https://py-googletrans.readthedocs.io/en/latest/
-
-#Example:
+# This script's goal is to decode any hex data and translate it to english.
+# Python3: It's also necessary to have libretranslatepy library installed found at: https://github.com/argosopentech/LibreTranslate-py
+# Example:
 #"d0 a2 d0 b0 d0 ba 20 d0 b4 d0 be d0 bb d0 b3 d0 be 20 d0 b8 20 d1 81 d0 bf d0 b0 d1 81 d0 b8 d0 b1 d0 be 20 d0 b7 d0 b0 20 d0 b2 d1 81 d0 b5 20 d1 80 d1 8b d0 b1 d1 8b 2e"
 
 ##########################################
@@ -18,10 +15,7 @@ translated=""
 function decoder {
     hex_data=$1
     echo "Decoding $hex_data . . ."
-    decoded_hex=$(echo "$hex_data" | node cyberchef-decode.js)
-    decoded=$decoded_hex
-    echo "Decoded hex data: $decoded_hex"
-    translated_hex=$(python3 googletranslator.py "$decoded_hex")
+    translated_hex=$(python3 libretranslator.py "$hex_data")
     translated=$translated_hex
 
     return 0
